383d2678189c3191e6c5b53639fe91f7ee55f6ce
Rafael Marcelo <rafael.2018.ads@gmail.com>
2021-11-15 18:54:30 -0300

    Middleware de verificacao do JWT

---
+++ src/middlewares/loginRequired.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+
+export default (req, res, next) => {
+  const {
+    authorization,
+  } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({
+      errorr: ['Login required.'],
+    });
+  }
+
+  const [, token] = authorization.split(' ');
+
+  try {
+    const dados = jwt.verify(token, process.env.TOKEN_SECRET);
+    const {
+      id,
+      email,
+    } = dados;
+
+    req.userId = id;
+    req.userEmail = email;
+    return next();
+  } catch (error) {
+    return res.status(401).json({
+      errorr: ['Token expirado ou inválido.'],
+    });
+  }
+};

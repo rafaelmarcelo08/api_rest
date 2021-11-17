91d5957739c899428ad1735b7c806127839988cf
Rafael Marcelo <rafael.2018.ads@gmail.com>
2021-11-17 20:02:36 -0300

    Salvando a foto na base de dados

--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -3,9 +3,10 @@ import {
 } from 'express';
 
 import fotoController from '../controllers/FotoController';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
-router.post('/', fotoController.store);
+router.post('/', loginRequired, fotoController.store);
 
 export default router;
